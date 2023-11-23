60dec4e35ad9d56a906455635bb0a00db497fa32
Kirito <2493381254@qq.com>
2023-11-23 14:26:51 +0800

    7

--- 双指针/2_pointer.go
+++ 双指针/2_pointer.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 // 4.
 // 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
 //
@@ -15,4 +17,107 @@ func moveZeroes(nums []int) {
 			l++
 		}
 	}
+}
+
+// 5.
+// 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+//
+// 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+//
+// 返回容器可以储存的最大水量。
+//
+// 说明：你不能倾斜容器。
+func maxArea(height []int) int {
+	left, right := 0, len(height)-1
+	ans := 0
+	for left < right {
+		h := min(height[left], height[right])
+		l := right - left
+		ans = max(ans, h*l)
+		if height[left] <= height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return ans
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 6.
+// 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]]
+// 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+//
+// 你返回所有和为 0 且不重复的三元组。
+//
+// 注意：答案中不可以包含重复的三元组。
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举 a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同 --> 用于去重
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同 --> 用于去重
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，
+			//可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+
+// 7.
+// 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+func trap(height []int) int {
+	left, right := 0, len(height)-1
+	leftMax, rightMax := 0, 0
+	ans := 0
+	for left < right {
+		leftMax = max(leftMax, height[left])
+		rightMax = max(rightMax, height[right])
+		if height[left] < height[right] {
+			ans += leftMax - height[left]
+			left++
+		} else {
+			ans += rightMax - height[right]
+			right--
+		}
+	}
+	return ans
 }
